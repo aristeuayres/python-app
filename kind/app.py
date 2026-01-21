c5b215edde8472eb9ef3536d78c2dc892aefce4a
aristeuayres <50341908+aristeuayres@users.noreply.github.com>
2026-01-21 17:49:38 -0300

    Update app.py

--- kind/app.py
+++ kind/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%I:%M:%S%p on %B"),
         'hostname': socket.gethostname()
-        'message': 'Change !!'
+        'message': 'Change'
 
     })
 
